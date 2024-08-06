0830e6d3c0819650742608e1cd3f0ac8cfeccee0
itallume <itallo.oliver21@gmail.com>
2024-08-06 00:40:44 -0300

    implementação do metodo findById da classe DepartmentDaoJDBC

--- src/models/dao/impl/DepartmentDaoJDBC.java
+++ src/models/dao/impl/DepartmentDaoJDBC.java
@@ -1,12 +1,22 @@
 package models.dao.impl;
 
+import db.DB;
+import db.DbException;
 import models.dao.DepartmentDao;
 import models.entities.Department;
 
+import java.sql.*;
 import java.util.List;
 
 
 public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Department obj) {
         
@@ -24,8 +34,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public Department findById(Integer id) {
-        return null;
-    }
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("select * from department where Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()){
+                Department department = new Department(rs.getString("Name") ,rs.getInt("Id"));
+                return department;
+            }
+            return null;
+        } catch (SQLException e) {
+        throw new DbException(e.getMessage());
+    } finally {
+        DB.closeStatement(st);
+        DB.closeResultSet(rs);
+    }}
 
     @Override
     public List<Department> findAll() {
